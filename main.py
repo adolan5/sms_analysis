e95d96c69937868de3b5ad434badb175a8e27706
Andy Dolan <20274393+adolan5@users.noreply.github.com>
2020-02-20 00:57:44 -0700

    Update main run script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import spacy
 from smsanalysis import MessageAnalyzer
 
 logging.basicConfig()
@@ -6,6 +7,11 @@ logging.getLogger('smsanalysis').setLevel(logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+logger.debug('Loading spacy model')
+# spacy_model = spacy.load('en_core_web_lg', disable=['parse', 'ner'])
+spacy_model = spacy.load('en_core_web_sm', disable=['parse', 'ner'])
+
+logger.debug('Creating MessageAnalyzer')
 ma = MessageAnalyzer('./data/sms-export.xml')
 # ma = message_analyzer('./sms-text-only.xml')
 logger.info(ma)
