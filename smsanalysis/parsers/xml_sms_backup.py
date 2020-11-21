c08407e78b3c441c50cf537555c0a8ca4a36f76b
Andy Dolan <20274393+adolan5@users.noreply.github.com>
2020-11-21 00:43:20 -0700

    Move XML import to new XMLParser

--- smsanalysis/parsers/xml_sms_backup.py
+++ smsanalysis/parsers/xml_sms_backup.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from xml.etree import ElementTree as ET
 from smsanalysis.parsers import Parser
 
 logger = logging.getLogger(__name__)
@@ -11,4 +12,15 @@ class XMLParser(Parser):
 
     def _import_messages(self, sms_source):
         logger.debug('Importing SMS data from SMS Backup XML')
-        return []
+        if type(sms_source) is not str:
+            logger.error('sms_source is expected to be a path to an sms export')
+        logger.debug('Attempting to import SMS messages from {}'.format(sms_source))
+        if not os.path.exists(sms_source):
+            logger.error('Export {} does not exist'.format(sms_source))
+            raise Exception('Argument must be a path to an sms export')
+        tree = ET.parse(sms_source)
+        root = tree.getroot()
+        # TODO: This currently does not correctly pull data from MMS messages -
+        # those need to be handled differently
+        messages_list = [dict(m.items()) for m in root]
+        return messages_list
