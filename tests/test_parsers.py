c1c6f1d06b49ce86257c278e84b85e196f1ec700
Andy Dolan <20274393+adolan5@users.noreply.github.com>
2021-01-31 11:33:23 -0700

    Update parser tests

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -2,9 +2,14 @@ import unittest
 from smsanalysis.parsers import XMLParser
 
 class TestParser:
-    def test_read_data(self):
-        self.assertEqual(5, len(self.parser.messages_list))
+    def test_read_num_messages(self):
+        self.assertEqual(5, len(self.messages))
+
+    def test_message_ordering(self):
+        pass
+        # self.assertEqual("This is a text message.", self.messages)
 
 class TestXMLParser(unittest.TestCase, TestParser):
     def setUp(self):
-        self.parser = XMLParser('./data/tests/sms-backup.xml')
+        self.parser = XMLParser()
+        self.messages = self.parser.read_messages('./data/tests/sms-backup.xml')
